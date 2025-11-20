f0110fe21188d30f85ce9d896cfa326672b36d56
Essi Kaukometsa <ac7793@student.jamk.fi>
2025-11-20 14:09:17 +0200

    Lisätty asiaa app.test.js tiedostoon

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import laskin from '../public/app.js';
+
+describe('Taskin luonnin testaus', function () {
+  it('should do something', function () {});
+
+  it('should do other stuff', function () {});
+});
